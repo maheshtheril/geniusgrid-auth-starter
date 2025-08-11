757a1df9be48e1daf087abbbd2013cac5b848475
Mahesh Theril <maheshtheril@live.com>
2025-08-11 09:49:22 +0530

    Quick update

--- frontend/vite.config.cjs
+++ frontend/vite.config.cjs
@@ -4,10 +4,9 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
