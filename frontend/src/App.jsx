33362cc6cb05e87ee8e55b7690b1135cda241a0b
Mahesh Theril <maheshtheril@live.com>
2025-08-22 22:13:54 +0530

    Quick update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import "./index.css";
 import "./styles.css";
 import "./styles/theme.css";
-import "./styles/sidebar-skin.css";
+
 import { Routes, Route, Navigate, Link, Outlet } from "react-router-dom";
 
 /* -------- Lazy: Public pages -------- */
